2f74b9a5bc89658089aa09727bd8ad5bc579c199
Lou <81883479+lawst-code@users.noreply.github.com>
2025-08-15 16:27:53 +0100

    adding a node

--- app/a_proper_one/a_proper_one.py
+++ app/a_proper_one/a_proper_one.py
@@ -18,3 +18,17 @@ class AProperOneNode(Node):
     def call(self, input_text: str) -> Dict:
         # Plugin logic here
         return {"result": input_text}
+
+class AnEvenMoreProperOneNode(Node):
+    name = "an_even_more_proper_one-node"
+    title = "Does something interesting with two numbers"
+    description = "Another node for a_proper_one"
+
+    def call(self, input_a: int, input_b: int) -> Dict:
+        # Plugin logic here
+        if input_a % 2 == 0:  # input_a is even
+            result = input_a * input_b
+        else:  # input_a is odd
+            result = input_a + input_b
+
+        return {"result": result}
